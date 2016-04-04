ecfc83a44e70d263de88098b23d415e8e16d94f6
Clang0 <clancyhu@tcd.ie>
2016-04-04 20:05:22 +0100

    Merged mine and eddies register pages
    merged the css and php files

--- ExchangeShop/register.php
+++ ExchangeShop/register.php
@@ -26,37 +26,68 @@ if(isset($_POST['btn-signup']))
  }
 }
 ?>
-<!DOCTYPE html PUBLIC>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Login Registration System</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
+<!DOCTYPE html>
+<html lang="en">
 
-</head>
-<body>
-<center>
-<div id="login-form">
-<form method="post">
-<table align="center" width="30%" border="0">
-<tr>
-<td><input type="text" name="uname" placeholder="User Name" required /></td>
-</tr>
-<tr>
-<td><input type="email" name="email" placeholder="Your Email" required /></td>
-</tr>
-<tr>
-<td><input type="password" name="pass" placeholder="Your Password" required /></td>
-</tr>
-<tr>
-<td><button type="submit" name="btn-signup">Sign Me Up</button></td>
-</tr>
-<tr>
-<td><a href="../index.php">Sign In Here</a></td>
-</tr>
-</table>
-</form>
-</div>
-</center>
-</body>
+    <head>
+
+        <meta charset="utf-8">
+        <title>Fullscreen Responsive Register Template</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- CSS -->
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=PT+Sans:400,700'>
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Oleo+Script:400,700'>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/styleregister.css" type="text/css">
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body>
+
+        <div class="register-container container">
+            <div class="row">
+                <div class="iphone span5">
+                    <img src="assets/img/africanmask.png" alt="">
+                </div>
+                <div class="register span6">
+                    <form method="post">
+                    <table align="center" width="30%" border="0">
+                    <tr>
+                    <td><input type="text" name="uname" placeholder="User Name" required /></td>
+                    </tr>
+                    <tr>
+                    <td><input type="email" name="email" placeholder="Your Email" required /></td>
+                    </tr>
+                    <tr>
+                    <td><input type="password" name="pass" placeholder="Your Password" required /></td>
+                    </tr>
+                    <tr>
+                    <td><button type="submit" name="btn-signup">Sign Me Up</button></td>
+                    </tr>
+                    <tr>
+                    <td><a href="../index.php">Sign In Here</a></td>
+                    </tr>
+                    </table>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- Javascript -->
+        <script src="assets/js/jquery-1.8.2.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+
+    </body>
+
 </html>
+
